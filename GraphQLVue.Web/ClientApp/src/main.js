e1856535190c9174144bdd73421d686e45157603
Bishwaranjan Sandhu <bishwaranjan187@yahoo.in>
2021-03-16 20:20:12 +0100

    Added apolo client

--- GraphQLVue.Web/ClientApp/src/main.js
+++ GraphQLVue.Web/ClientApp/src/main.js
@@ -3,4 +3,26 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import VueApollo from "vue-apollo";
+
+import ApolloClient from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+// Create an http link:
+const link = new HttpLink({
+    uri: 'http://localhost:14995/graphql',
+    fetch
+});
+const client = new ApolloClient({
+    link: link,
+    cache: new InMemoryCache({
+        addTypename: true
+    })
+});
+
+const apolloProvider = new VueApollo({
+    defaultClient: client,
+})
+
+createApp(App).provide(apolloProvider).use(router).mount('#app')
