c3501e068bc9d1567677a92df37ceee526f7de91
Bishwaranjan Sandhu <bishwaranjan187@yahoo.in>
2021-03-17 10:55:30 +0100

    Implemented tech events nav tab

---
+++ GraphQLVue.Web/ClientApp/src/components/TechEvents.vue
@@ -0,0 +1,47 @@
+<template>
+   <h1 id="tableLabel">Tech Events</h1>
+
+    <p>This component demonstrates fetching data from the GraphQL.</p>
+
+    <p v-if="!techEvents"><em>Loading...</em></p>
+
+    <table class='table table-striped' aria-labelledby="tableLabel" v-if="techEvents">
+        <thead>
+            <tr>
+                <th>Event Name</th>
+                <th>Speaker</th>
+                <th>Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="techEvent of techEvents" v-bind:key="techEvent">
+                <td>{{ techEvent.eventName }}</td>
+                <td>{{ techEvent.speaker }}</td>
+                <td>{{ techEvent.eventDate }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+        name: "TechEvents",
+        data() {
+            return {
+                techEvents: []
+            }
+        },
+        methods: {
+            getTechEvents() {
+                this.techEvents.push({eventId:1,eventName:'Bish Demo',speaker:'Bishwaranjan Sandhu',eventDate:'2021-12-12'});
+            }
+        },
+        mounted() {
+            this.getTechEvents();
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ GraphQLVue.Web/ClientApp/src/router/index.js
@@ -0,0 +1,35 @@
+﻿import { createWebHistory, createRouter } from "vue-router";
+import Home from "@/components/Home.vue";
+import Counter from "@/components/Counter.vue";
+import FetchData from "@/components/FetchData.vue";
+import TechEvents from "@/components/TechEvents.vue";
+
+const routes = [
+    {
+        path: "/TechEvents",
+        name: "TechEvents",
+        component: TechEvents,
+    },
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/Counter",
+        name: "Counter",
+        component: Counter,
+    },
+    {
+        path: "/FetchData",
+        name: "FetchData",
+        component: FetchData,
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
